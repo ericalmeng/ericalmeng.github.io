14c203b6e4143893d4bbe208b70b9104b9bc0ad7
Erica Meng <ericalmeng@gmail.com>
2016-11-11 01:00:51 -0800

    fix hovering after animation in projects

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,22 @@ $(document).ready(function() {
 
  	//add class for social media
  	$(".home-social_media").addClass("clear");
+
+ 	//hovering for project
+	$(".project-box").hover(function() {
+		$(this).removeClass("box1");
+		$(this).removeClass("box2");
+		$(this).removeClass("box3");
+		$(this).removeClass("box4");
+		$(this).css("-webkit-transition:", "linear 0.2s");
+		$(this).css("-moz-transition:", "linear 0.2s");
+		$(this).css("-o-transition:", "linear 0.2s");
+		$(this).css("-transition-transition:", "linear 0.2s");
+		$(this).css("opacity", "1");
+		$(this).css("cursor", "pointer");
+		}, function() {
+		$(this).css("opacity", "0.7");
+	})
 	
  	//hovering in nav
 	$(".home-title .links-container .link").hover(function() {
@@ -23,23 +39,8 @@ $(document).ready(function() {
 		$(".projects").show();
 	})
 
-	// when click on drawings, hide everything else
-	// $("#drawings").click(function() {
-	// 	$(".about-text").hide();
-	// 	$(".projects").hide();
-	// 	$(".resume").hide();
-	// 	$(".contact").hide();
-	// 	$(".drawings").show();
-	// })
-
 	// when click on resume, hide everything else
 	$("#resume").click(function() {
-		// if ($(window).width() <= 720) {
-  //  			$(".resume img").hide();
-  //  			$(".resume a").show();
-		// } else {
-		// 	$(".resume").show();
-		// }
 		$(".resume").show();
 		$(".about-text").hide();
 		$(".drawings").hide();
@@ -56,33 +57,6 @@ $(document).ready(function() {
 		$(".contact").show();
 	})
 
-	//check url try using switch statement but it didn't work
-	// var param;
-
-	// switch(window.location.hash.indexOf(param) > -1) {
- //    case "#projects":
- //        $(".about-text").hide();
- //        $(".projects").show();
- //        break;
- //    case "#drawings":
- //        $(".about-text").hide();
- //        $(".drawings").show();
- //        break;
- //    case "#resume":
- //        $(".about-text").hide();
- //        $(".resume").show();
- //        break;
- //    case "#contact":
- //        $(".about-text").hide();
- //        $(".contact").show();
- //        break;
- //    default:
- //        $(".projects").hide();
-	// 	$(".drawings").hide();
-	// 	$(".resume").hide();
-	// 	$(".contact").hide();
-	// }
-
 	//check url
 	if (window.location.hash.indexOf("#projects") > -1) {
 		$(".projects").show();
@@ -91,20 +65,8 @@ $(document).ready(function() {
 		$(".contact").hide();
 		$(".about-text").hide();
 	}
-	// if (window.location.hash.indexOf("#drawings") > -1) {
-	// 	$(".drawings").show();
-	// 	$(".projects").hide();
-	// 	$(".resume").hide();
-	// 	$(".contact").hide();
-	// 	$(".about-text").hide();
-	// }
+
 	if (window.location.hash.indexOf("#resume") > -1) {
-		// if ($(window).width() <= 720) {
-  //  			$(".resume img").hide();
-  //  			$(".resume a").show();
-		// } else {
-		// 	$(".resume").show();
-		// }
 		$(".resume").show();
 		$(".drawings").hide();
 		$(".projects").hide();
