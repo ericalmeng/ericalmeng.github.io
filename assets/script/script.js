55b0dcae7da2cacfcc4745ea19956aeacb83b290
Erica Meng <ericalmeng@gmail.com>
2016-11-15 19:41:32 -0800

    fix conflict names

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	})
 
 	// when click on resume, hide everything else
-	$("#resume").click(function() {
+	$("#r").click(function() {
 		$(".resume").show();
 		$(".about-text").hide();
 		$(".projects").hide();
@@ -33,7 +33,7 @@ $(document).ready(function() {
 	})
 
 	// when click on contact, hide everything else
-	$("#contact").click(function() {
+	$("#c").click(function() {
 		$(".about-text").hide();
 		$(".projects").hide();
 		$(".resume").hide();
@@ -50,14 +50,14 @@ $(document).ready(function() {
 		// location.load();
 	}
 
-	if (window.location.hash.indexOf("#resume") > -1) {
+	if (window.location.hash.indexOf("#r") > -1) {
 		$(".resume").show();
 		$(".projects").hide();
 		$(".contact").hide();
 		$(".about-text").hide();
 		// location.load();
 	}
-	if (window.location.hash.indexOf("#contact") > -1) {
+	if (window.location.hash.indexOf("#c") > -1) {
 		$(".contact").show();
 		$(".projects").hide();
 		$(".resume").hide();
